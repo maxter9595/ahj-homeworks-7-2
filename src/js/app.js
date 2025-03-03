f46a9141a135a86af3caf1d3f952e3875341331d
Maxim Terletskii <max.t95@bk.ru>
2025-03-03 22:02:21 +0300

    Use github actions

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import ImageGallery from "./ImageGallery.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const baseUrl = "http://localhost:3000";
-  // const baseUrl = "https://your-render-app-url.onrender.com";
+  // const baseUrl = "http://localhost:3000";
+  const baseUrl = "https://ahj-homeworks-7-2.onrender.com";
   new ImageGallery("#app", baseUrl);
 });
